9ba5ea4d70b0e6da5f2e89a73b20c6f7dc2c6788
Rob de Jonge <rob.dejonge@gmail.com>
2020-11-21 17:56:35 +0700

    Toggle any Home Assistant automation on or off
    Added an option that lists all automations from the targeted Home Assistant installation, and when selected toggles that automation on or off. Note, this does not trigger the automation.

--- Home Assistant.lbaction/Contents/Scripts/default.js
+++ Home Assistant.lbaction/Contents/Scripts/default.js
@@ -87,6 +87,56 @@ function runScript(entity_id) {
   }
 }
 
+
+function listAutomations() {
+
+  result = getAPI('states');
+
+  var theAutomations = [];
+
+  for(let j = 0; j < result.data.length; j++) {
+
+    if (result.data[j].entity_id.split(".")[0] === "automation") { 
+
+      theAutomations.push({
+          title: result.data[j].attributes.friendly_name,
+          icon: 'home-assistant-logo.png',
+          action: 'toggleAutomation',
+          actionArgument: result.data[j].entity_id,
+          actionReturnsItems: false,
+          actionRunsInBackground: true
+      });
+    }
+  }
+
+  return theAutomations;
+}
+
+function toggleAutomation(entity_id) {
+  
+  action = "toggle " + entity_id.split(".")[1] + " automation";
+
+  service_data = { 
+    'entity_id': entity_id
+  };
+
+  result = postAPI_JSON('services/automation/toggle', service_data);
+  
+  if (anyCommonErrors(result, action)) {
+
+    // already did what needed to be done
+
+  } else {
+
+    LaunchBar.displayNotification({
+
+      title  : discovery_info.location_name + ' (Home Assistant ' + discovery_info.version + ')',
+      string : action.charAt(0).toUpperCase() + action.substring(1) + ' complete'
+
+    })
+  }
+}
+
 function executeAPICall(my) {
 
   if (my.method == "post") {
@@ -137,6 +187,14 @@ function run(argument) {
     action: 'listScripts',
     actionReturnsItems: true
   });
+
+  actions.push({
+    title: 'Toggle an automation',
+    icon: 'home-assistant-logo.png',
+    action: 'listAutomations',
+    actionReturnsItems: true
+  });
+
   
   for(let i = 0; i < settings.API_calls.length; i++){
 
